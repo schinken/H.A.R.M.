b87bdaeff82dfb8c8a8e61cf71d86a9ba9e36923
Schinken <schinken@hackerspace-bamberg.de>
2012-09-30 00:05:23 +0200

    added config params

--- config.example.js
+++ config.example.js
@@ -5,6 +5,9 @@ module.exports = {
        'host': 'door.bckspc.de',
        'path': '/verify',
        'pass': '---whatever---'
-    }
+    },
+    'arduino_device': '/dev/ttyACM0',
+    'snmp_host': '10.1.20.1',
+    'status_api': 'http://status.bckspc.de/status.php?response=json'
     
 };
